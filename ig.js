2c37d2db9ddaa7859b864f4e56f50838ce73b78b
rafiherfanda5 <40054399+rafiherfanda5@users.noreply.github.com>
2018-06-08 14:19:15 +0700

    Update ig.js
    deleting following function

--- ig.js
+++ ig.js
@@ -139,8 +139,7 @@ const CommentAndLike = async function(session, accountId, text){
 			ngeComment(session, result[0].params.id, text),
 			ngeLike(session, result[0].params.id)
 		]
-		const [Follow,Comment,Like] = await Promise.all(task);
-		const printFollow = Follow ? chalk`{green Follow}` : chalk`{red Follow}`;
+		const [Comment,Like] = await Promise.all(task);
 		const printComment = Comment ? chalk`{green Comment}` : chalk`{red Comment}`;
 		const printLike = Like ? chalk`{green Like}` : chalk`{red Like}`;
 		return chalk`{bold.green ${printFollow},${printComment},${printLike} [${text}]}`;
